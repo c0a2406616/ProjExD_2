a03d1a9cd9eeb7c591a09fd22c2669ca98625aef
c0a2406616 <c0a2406616@edu.teu.ac.jp>
2025-04-22 18:00:04 +0900

    修正完了 #2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,7 +14,7 @@ def check_bound(rct:pg.Rect) -> tuple[bool,bool]:
     画面内ならTrue,画面買いならFalse
     """
     yoko, tate = True, True
-     #横縦方向用の変数
+    # 横縦方向用の変数
     # 横方向判定
     if rct.left < 0 or WIDTH < rct.right:  # 画面ないだったら
         yoko = False
@@ -27,10 +27,10 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     DELTA = {
-        pg.K_UP:(0,-5),
-        pg.K_DOWN:(0,+5),
-        pg.K_LEFT:(-5,0),
-        pg.K_RIGHT:(+5,0),
+        pg.K_UP:(0, -5),
+        pg.K_DOWN:(0, +5),
+        pg.K_LEFT:(-5, 0),
+        pg.K_RIGHT:(+5, 0),
     }
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
